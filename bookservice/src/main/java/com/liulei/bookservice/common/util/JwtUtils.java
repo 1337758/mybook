d6960a7ff4dda582c5bc6e4178745fe419b52b86
liulei <laurunzhe@163.com>
2019-06-20 10:38:35 +0800

    2

--- bookservice/src/main/java/com/liulei/bookservice/common/util/JwtUtils.java
+++ bookservice/src/main/java/com/liulei/bookservice/common/util/JwtUtils.java
@@ -1,10 +1,12 @@
 package com.liulei.bookservice.common.util;
 
 import com.auth0.jwt.JWTSigner;
+import com.auth0.jwt.JWTVerifier;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class JwtUtils {
 
@@ -18,6 +20,7 @@ public class JwtUtils {
 
     /**
      * 根据对象生成token
+     *
      * @param obj
      * @param <T>
      * @return
@@ -31,6 +34,34 @@ public class JwtUtils {
             map.put(EXP, System.currentTimeMillis() + expiresSecond);
             return jwtSigner.sign(map);
         } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+
+
+    /**
+     * 根据token解析对象
+     *
+     * @param classT
+     * @param jwt
+     * @param <T>
+     * @return
+     */
+    public static <T> T parse(Class<T> classT, String jwt) {
+        final JWTVerifier verifier = new JWTVerifier(SECRET);
+        try {
+            final Map<String, Object> claims = verifier.verify(jwt);
+            if (claims.containsKey(EXP) && claims.containsKey(HEADER)) {
+                long exp = (Long) claims.get(EXP);
+                long currentTimeMillis = System.currentTimeMillis();
+                if (exp > currentTimeMillis) {
+                    String json = (String) claims.get(HEADER);
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    return objectMapper.readValue(json, classT);
+                }
+            }
+            return null;
+        } catch (Exception e) {
             return null;
         }
     }
